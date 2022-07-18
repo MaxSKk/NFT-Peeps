4fa472e02491073b5814256350789cec0ae195d7
MaxSk <maxskokov4@gmail.com>
2022-07-18 17:57:28 +0300

    add collection section

--- js/script.js
+++ js/script.js
@@ -4,4 +4,11 @@ $(function () {
       $('body').toggleClass('lock')
    });
 
+   $('a[href*="#"]').on('click', function () {
+      $('html, body').animate({
+         scrollTop: $($.attr(this, 'href')).offset().top
+      }, 400);
+      return false;
+   });
+
 });
